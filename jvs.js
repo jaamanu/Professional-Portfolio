495d6d1547576f13d8a5deb97d01d901add3e33f
JAMES ASIBEY MANU <jaamanu@gmail.com>
2022-09-09 19:35:12 +0200

    Fix linter erros

--- jvs.js
+++ jvs.js
@@ -29,13 +29,6 @@ navLink.forEach((link) => {
   });
 });
 
-
-
-
-
-
-
-
 const projects = [{
   projectTitle: 'Professional Art Printing Data',
   projectTitle2: 'Professional Art Printing Data',
@@ -65,7 +58,7 @@ const projects = [{
   technology: ['HTML', 'Bootstrap', 'CSS'],
   sourceCode: '',
   liveDemo: '',
-},{
+}, {
   projectTitle: 'Professional Art Printing Data',
   projectTitle2: 'Professional Art Printing Data',
   projectImage: './images/jamanu1png.png',
@@ -74,7 +67,7 @@ const projects = [{
   technology: ['HTML', 'Bootstrap', 'CSS'],
   sourceCode: '',
   liveDemo: '',
-},{
+}, {
   projectTitle: 'Professional Art Printing Data',
   projectTitle2: 'Professional Art Printing Data',
   projectImage: './images/jamanu1png.png',
@@ -83,7 +76,7 @@ const projects = [{
   technology: ['HTML', 'Bootstrap', 'CSS'],
   sourceCode: '',
   liveDemo: '',
-},{
+}, {
   projectTitle: 'Professional Art Printing Data',
   projectTitle2: 'Professional Art Printing Data',
   projectImage: './images/jamanu1png.png',
@@ -95,14 +88,13 @@ const projects = [{
 },
 ];
 
-
 const cards = document.querySelector('.grid');
 const popup = document.querySelector('.pop');
 
-projects.forEach((project) =>{
-const card = document.createElement('div');
-const button = document.createElement('div');
-card.innerHTML = `
+projects.forEach((project) => {
+  const card = document.createElement('div');
+  const button = document.createElement('div');
+  card.innerHTML = `
 <article class="card-article flex flex-col">
 <div class="card-block">
 <header class="article-title">
@@ -118,23 +110,23 @@ ${project.description}
 </ul>
 </div>
 </article>
-`
-card.classList.add('work-card')
-card.classList.add('flex')
-card.classList.add('flex-col')
-card.classList.add('end-col-left')
-card.append(button)
+`;
+  card.classList.add('work-card');
+  card.classList.add('flex');
+  card.classList.add('flex-col');
+  card.classList.add('end-col-left');
+  card.append(button);
 
-button.innerHTML =`
+  button.innerHTML = `
 <p>see project</p>
-`
-button.classList.add('card-btn')
-button.classList.add('button')
+`;
+  button.classList.add('card-btn');
+  button.classList.add('button');
 
-button.addEventListener('click', () => {
-  popup.style.display = 'block'
-  document.body.style.overflow = 'hidden';
-  popup.innerHTML = `
+  button.addEventListener('click', () => {
+    popup.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+    popup.innerHTML = `
   <div class="modal">
   <div class="second flex align justbtw">
       <h2 class="modHeader">${project.projectTitle2}</h2>
@@ -163,48 +155,46 @@ button.addEventListener('click', () => {
   </div>
 </div>
 </div>
-  `
-  const btnPop = document.querySelector('.burg')
-btnPop.addEventListener('click', () => {
-  popup.style.display = 'none'
-  document.body.style.overflow = 'initial';
-})
-  
+  `;
+    const btnPop = document.querySelector('.burg');
+    btnPop.addEventListener('click', () => {
+      popup.style.display = 'none';
+      document.body.style.overflow = 'initial';
+    });
+  });
+  cards.append(card);
 });
-cards.append(card)
-});
 
-
-
-const emailError = document.querySelector('.spanMail')
-const submitError = document.querySelector('.spanSubmit')
+const emailError = document.querySelector('.spanMail');
+const submitError = document.querySelector('.spanSubmit');
 
 function emailVal() {
-  const email = document.querySelector('.email').value
+  const email = document.querySelector('.email').value;
 
-  if (email.length === 0 ) {
-    emailError.innerHTML='Required'
-    emailError.style.color = 'red'
+  if (email.length === 0) {
+    emailError.innerHTML = 'Required';
+    emailError.style.color = 'red';
     return false;
   }
   if (!email.match(/^[a-z-0-9.!#$%&'+/=?^_`{|}~-]+@[a-z-0-9-]\.[a-z]+(?:\.[a-z-0-9-]+)*$/)) {
-    emailError.innerHTML='lowercase only'
-    emailError.style.color = 'red'
+    emailError.innerHTML = 'lowercase only';
+    emailError.style.color = 'red';
     return false;
   }
-  emailError.innerHTML = 'correct'
-  emailError.style.color = 'green'
+  emailError.innerHTML = 'correct';
+  emailError.style.color = 'green';
   return true;
 }
 
 function validateForm() {
   if (!emailVal()) {
-    submitError.innerHTML = 'fix errors'
+    submitError.innerHTML = 'fix errors';
     setTimeout(() => { submitError.style.display = 'none'; }, 4000);
     return false;
   }
+  return true;
 }
-
+validateForm();
 const Name = document.querySelector('.name');
 const NameValue = localStorage.getItem('Name');
 
